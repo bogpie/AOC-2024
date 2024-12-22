1e9de62534c790fd5dbaafed852fdaa8c54e3cbf
bogpie <bogdanpielemail@gmail.com>
2024-12-22 22:41:01 +0200

    d22 part 2

--- src/D22/D22.java
+++ src/D22/D22.java
@@ -1,18 +1,17 @@
 package D22;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class D22 {
 
     public static final int MODULO = 16777216;
+    public static final int DEPTH = 2000;
 
     public void main() {
-
-        ArrayList<Long> secrets = new ArrayList<>();
-        try {
-            Scanner sc = new Scanner(new File("src/D22/input.txt"));
+        List<Long> secrets = new ArrayList<>();
+        try (Scanner sc = new Scanner(new File("src/D22/input.txt"))) {
             while (sc.hasNextLong()) {
                 secrets.add(sc.nextLong());
             }
@@ -20,39 +19,104 @@ public class D22 {
             e.printStackTrace();
         }
 
-        long sum = 0;
-        for (var secret : secrets) {
-            int depth = 2000;
-            long generated = generate(secret, depth);
-            System.out.println(generated);
-            sum += generated;
-        }
+        long sum = secrets.parallelStream()
+                .map(secret -> generateSecretData(secret))
+                .map(data -> data.secretHistory)
+                .mapToLong(secretHistory -> secretHistory[DEPTH - 1])
+                .sum();
+
         System.out.println(sum);
+
+        long best = secrets.parallelStream()
+                .map(secret -> generateSecretData(secret))
+                .flatMap(data -> data.deltasToPrice.entrySet().stream())
+                .collect(
+                        Collectors.toConcurrentMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                Long::sum
+                        )
+                )
+                .entrySet()
+                .stream()
+                //.peek(System.out::println)
+                // get values
+                .map(Map.Entry::getValue)
+                .max(Long::compare)
+                .orElse(0L);
+
+        System.out.println(best);
     }
 
-    long generate(long secret, int depth) {
-        for (int i = 0; i < depth; i++) {
-            long secretMultiplied = secret * 64;
-            secret = mix(secret, secretMultiplied);
-            secret = prune(secret);
+    private static SecretData generateSecretData(long secret) {
+        Map<Sequence, Long> deltasToPrice = new HashMap<>();
+        long[] deltas = new long[2001];
+        long[] secretHistory = new long[2001];
 
-            var secretDivided = secret / 32;
-            secret = mix(secret, secretDivided);
-            secret = prune(secret);
+        secretHistory[0] = secret;
+        for (int i = 0; i < DEPTH + 1; i++) {
+            long oldSecret = secret;
 
-            secretMultiplied = secret * 2048;
-            secret = mix(secret, secretMultiplied);
-            secret = prune(secret);
+            secret = prune(mix(secret, secret * 64));
+            secret = prune(mix(secret, secret / 32));
+            secret = prune(mix(secret, secret * 2048));
+
+            deltas[i] = secret % 10 - oldSecret % 10;
+            secretHistory[i] = secret;
+
+            if (i >= 4) {
+                long[] sequenceDeltas = new long[4];
+                System.arraycopy(deltas, i - 4, sequenceDeltas, 0, 4);
+                Sequence sequence = new Sequence(sequenceDeltas);
+                deltasToPrice.putIfAbsent(sequence, oldSecret % 10);
+            }
         }
 
-        return secret;
+        return new SecretData(deltasToPrice, secretHistory);
     }
 
-    long mix(long secret, long value) {
+    private static long mix(long secret, long value) {
         return value ^ secret;
     }
 
-    long prune(long secret) {
+    private static long prune(long secret) {
         return secret % MODULO;
+    }
+
+    private static class SecretData {
+        Map<Sequence, Long> deltasToPrice;
+
+        long[] secretHistory;
+
+        SecretData(Map<Sequence, Long> deltasToPrice, long[] secretHistory) {
+            this.deltasToPrice = deltasToPrice;
+            this.secretHistory = secretHistory;
+        }
+    }
+
+    private static class Sequence {
+        @Override
+        public String toString() {
+            return Arrays.toString(deltas);
+        }
+
+        long[] deltas;
+
+        Sequence(long[] deltas) {
+            this.deltas = deltas;
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(deltas);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Sequence sequence = (Sequence) obj;
+            return Arrays.equals(deltas, sequence.deltas);
+        }
     }
 }
